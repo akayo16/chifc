323e374a13248504e1c75eca84cdbbfabd077867
Akayo16 <rinatgalin679@gmail.com>
2024-12-02 00:11:16 +0300

    add CMakeLists

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+//
+// Created by akayo16 on 01.12.2024
+//
+
+#include <iostream>
+
+int main()
+{
+
+    std::cout << "Hi! There is still a lot of code to write. On to implementation"  << std::endl;
+
+    return 0;
+}
